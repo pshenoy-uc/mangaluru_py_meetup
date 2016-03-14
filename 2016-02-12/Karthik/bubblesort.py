beb3ba544ed746d52fbd9fe12b937a25178d4b6a
kbhat93 <karthik@mangaloreinfotech.in>
2016-03-14 11:30:29 +0530

    comments

--- 2016-02-12/Karthik/bubblesort.py
+++ 2016-02-12/Karthik/bubblesort.py
@@ -1,3 +1,12 @@
+"""
+Author: K@rthik Bh@t K.
+Date: March 14, 2016 11:23 AM
+
+Last Commit: Added reverse sorting option
+Description: 
+This code manually sorts the items of a iterable by swapping items.
+The iterables can be a list or dict.
+"""
 from collections import OrderedDict
 
 
@@ -7,12 +16,13 @@ def bubblesort(iterable, reverse=False):
             for j in xrange(len(iterable)):
                 if reverse:
                     if iterable[I] > iterable[j]:
+                        # swap items
                         temp = iterable[I]
                         iterable[I] = iterable[j]
                         iterable[j] = temp
                 else:
                     if iterable[I] < iterable[j]:
-                        # swap values
+                        # swap items
                         temp = iterable[I]
                         iterable[I] = iterable[j]
                         iterable[j] = temp
@@ -29,5 +39,5 @@ def bubblesort(iterable, reverse=False):
 
 
 unsorted_dict = {72:6, 85:2, 32:4, 49: 7, 'a':42, 'anish':'ram'}
-#d = ['a', 1, 'v', 4, 2, 'karthik', 76] 
+# d = ['a', 1, 'v', 4, 2, 'karthik', 76] 
 print bubblesort(unsorted_dict, reverse=True) 
