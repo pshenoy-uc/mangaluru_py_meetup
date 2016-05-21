5176eb7b0cadc55fb17c2722683d7c9c75d81d11
Abhilash Joseph C <abhilash@mangaloreinfotech.in>
2016-05-21 14:59:56 +0530

    Added Setup Files

--- 2016-05-21/application/appserver.py
+++ 2016-05-21/application/appserver.py
@@ -5,9 +5,11 @@ from flask import request
 
 @application.route("/", methods=['GET'])
 def sample():
-    x = request.form.get('dd')
-    print request.args
-    return jsonify({'test': x})
+    return jsonify({'test': 'sample'})
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({'ERROR': 404})
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
--- 2016-05-21/envsetup.sh
+++ 2016-05-21/envsetup.sh
@@ -5,9 +5,10 @@ sudo apt-get install python-pip python-dev nginx -y
 # PIP has not -y option :(
 yes w | sudo pip install gunicorn flask
 
-sudo cp /home/codaxtr_user/pydemo/appserver.conf /etc/init/appserver.conf
+HOME=`pwd`
+sudo cp $HOME/2016-05-21/appserver.conf /etc/init/appserver.conf
 sudo start appserver
 
 sudo rm -rf /etc/nginx/sites-available/default
 
-sudo cp /home/codaxtr_user/pydemo/nginx_appserver.conf /etc/nginx/sites-available/default
+sudo cp $HOME/2016-05-21/nginx_appserver.conf /etc/nginx/sites-available/default
